a2e287aaf855e131d650b94202e74003af1a005c
Jack Hall <jhall@scalecomputing.com>
2025-09-14 16:29:29 -0500

    dev: README and output from test script

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pulumi Deletion Dependency problem
+
+To run this test, simply execute:
+```bash
+sh setup_and_run.sh
+```
--- setup_and_run.sh
+++ setup_and_run.sh
@@ -6,3 +6,11 @@ PULUMI_CONFIG_PASSPHRASE="" pulumi config set db_url "some.db"
 
 PULUMI_CONFIG_PASSPHRASE="" EXAMPLE_PROGRAM_NUMBER=1 pulumi up -y
 PULUMI_CONFIG_PASSPHRASE="" EXAMPLE_PROGRAM_NUMBER=2 pulumi up -y
+
+vm_power_state=$(sqlite3 some.db 'SELECT power_state FROM vms WHERE uid = 1;')
+if [ "$vm_power_state" == "running" ]; then
+    echo "VM is running as expected. RunningStateProvider restarted VM successfully."
+else
+    echo "Unexpected VM power state: $vm_power_state"
+    exit 1
+fi
